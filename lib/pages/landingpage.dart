d12944b8fe069d4e453a3f640edf2d2225e32f32
sankalp <sankalppandey696@gmail.com>
2024-11-17 23:36:01 +0530

    created design for landing page

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -12,8 +12,68 @@ class _LandingpageState extends State<Landingpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.only(
+          top: 150,
+          left: 20,
+        ),
         child: Column(
-          children: [Image.asset("groceryImage.jpg")],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("groceryImage.jpg"),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "order your\nfavourite fruits",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Eat fresh fruits and be healthy",
+              style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: Color(0xffffcc3f),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10))),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Next",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 27,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_outlined,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
