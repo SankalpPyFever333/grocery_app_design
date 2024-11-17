1e7fab1442314b27446611cfed15c287cc45f442
sankalp <sankalppandey696@gmail.com>
2024-11-18 03:01:48 +0530

    created product card design

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,19 +12,285 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
+      padding: EdgeInsets.only(top: 50, left: 20),
       child: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        "handWave.jpg",
+                        height: 55,
+                        width: 55,
+                        fit: BoxFit.cover,
+                      ),
+                      Text(
+                        "Hey, Alex",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Eat fresh fruits and\nbe healthy",
+                    style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    "BoyImage.png",
+                    height: 60,
+                    width: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 20, top: 5, bottom: 5, right: 20),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Color(0xfff5f5f5),
+                borderRadius: BorderRadius.circular(20)),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search grocery",
+                  suffixIcon: Icon(Icons.search_outlined)),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                "Hey , Alex",
+                "Top selling product",
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 20,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Text(
+                  "see all",
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
             ],
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 300,
+            // wee cannot write ListView directly as a child of such widgets like Column who do not have height constraint on its children.
+            // So , by wrapping it with Container and provide a any height(set according to the content of it)
+            // We can use SizedBox also
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 10, left: 10, right: 5),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(227, 240, 158, 34),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                          topLeft: Radius.circular(20))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Orange",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "₹80/kg",
+                        style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Image.asset(
+                          "orange.jpg",
+                          height: 160,
+                          width: 160,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 115),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                        decoration: BoxDecoration(
+                            color: Color(0xffffeab5),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Icon(
+                          Icons.add,
+                          color: const Color.fromARGB(227, 240, 158, 34),
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10, left: 10, right: 5),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(226, 240, 34, 34),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                          topLeft: Radius.circular(20))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Apple",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "₹120/kg",
+                        style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Image.asset(
+                          "apple1.jpg",
+                          height: 160,
+                          width: 160,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 115),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                        decoration: BoxDecoration(
+                            color: Color(0xffffeab5),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Icon(
+                          Icons.add,
+                          color: const Color.fromARGB(226, 240, 34, 34),
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10, left: 10, right: 5),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(227, 240, 158, 34),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                          topLeft: Radius.circular(20))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Orange",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "₹80/kg",
+                        style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Image.asset(
+                          "orange.jpg",
+                          height: 160,
+                          width: 160,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 115),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                        decoration: BoxDecoration(
+                            color: Color(0xffffeab5),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Icon(
+                          Icons.add,
+                          color: const Color.fromARGB(227, 240, 158, 34),
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     ));
