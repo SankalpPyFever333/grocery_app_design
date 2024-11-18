06b174ca10241a7beb91de88b4f56837793e7701
sankalp <sankalppandey696@gmail.com>
2024-11-19 03:46:14 +0530

    designing prodpage.dart

--- lib/pages/detailprodpage.dart
+++ lib/pages/detailprodpage.dart
@@ -13,18 +13,177 @@ class _ProductDetailState extends State<ProductDetail> {
     return Scaffold(
       body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               children: [
                 Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 4,
+                  padding: EdgeInsets.only(top: 40, left: 20),
                   decoration: BoxDecoration(
-                    color: Color(0xffffe08e),
-                    
+                      color: Color(0xffffe08e),
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(40),
+                          bottomLeft: Radius.circular(40))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(Icons.arrow_back_ios_new)),
+                    ],
                   ),
-                  child: Text(""),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(140),
+                      child: Material(
+                        elevation: 4,
+                        child: Image.asset(
+                          "orange.jpg",
+                          height: 300,
+                          width: 300,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Orange-medium",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Oranges are a good source of vitamin C, as well as sugar, carotenoids, flavonoids, essential oils, and minerals. They can help with digestion, regulate blood sugar, and keep bowel movements regular.",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Color(0xfff5f5f5),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Icon(
+                          Icons.alarm,
+                          size: 40,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Delivery time",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "25-30 min",
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "₹80/kg",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(right: 15),
+                        margin: EdgeInsets.only(right: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            Icon(Icons.remove),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "01",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Icon(Icons.add),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(top: 8, bottom: 8),
+                    decoration: BoxDecoration(
+                        color: Color(0xffffe08e),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: Text(
+                        "Add to cart",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
